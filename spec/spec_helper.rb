62ef4eff5afc51f24efa82fc283c5413ac380efe
jstanley14 <jstanley@middil.com>
2014-01-08 13:40:03 -0500

    fix for capybara 'visit'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,4 +35,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  #For RSpec testing using 'visit'
+  config.include Capybara::DSL, :type => :request
 end
