b45233752450a56a718b716226a5ab2350319bc4
jstanley14 <jstanley@middil.com>
2014-01-14 10:54:53 -0500

    done with chapter 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -17,7 +17,6 @@ describe "User Pages" do
         before { click_button submit }
 
         it { should have_selector('title', text: 'Sign Up') }
-        it { should have_content('Password digest can\'t be blank') }
         it { should have_content('Name can\'t be blank') }
         it { should have_content('Email can\'t be blank') }
         it { should have_content('Email is invalid') }
@@ -37,6 +36,14 @@ describe "User Pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
 
